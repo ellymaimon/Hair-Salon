8d54091220ac4ab92892313e49f52b4ff55d1635
Elly Maimon <eliranmaimon@gmail.com>
2018-07-13 15:05:05 -0700

    Add Client model with  constructor, get all, and delete all methods

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -7,5 +7,71 @@ namespace HairSalon.Models
 {
     public class Client
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int StylistId { get; set; }
+
+        public Client(string name, string gender, int id = 0, int stylistId = 0)
+        {
+            Name = name;
+            Gender = gender;
+        }
+
+        public override bool Equals(System.Object otherClient)
+        {
+            if (!(otherClient is Client))
+            {
+                return false;
+            }
+            else
+            {
+                Client newClient = (Client)otherClient;
+                bool nameEquality = (this.Name == newClient.Name);
+                bool genderEquality = (this.Gender == newClient.Gender);
+                return (nameEquality && genderEquality);
+            }
+        }
+
+        public static List<Client> GetAll()
+        {
+            List<Client> allClients = new List<Client>() { };
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM clients;";
+            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while (rdr.Read())
+            {
+                int id = rdr.GetInt32(0);
+                string name = rdr.GetString(1);
+                string gender = rdr.GetString(2);
+                int stylistId = rdr.GetInt32(3);
+                Client newClient = new Client(name, gender, id, stylistId);
+                allClients.Add(newClient);
+            }
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return allClients;
+        }
+
+        public static void DeleteAll()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+
+            cmd.CommandText = @"DELETE FROM clients;";
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }           
     }
 }
