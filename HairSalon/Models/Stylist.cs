4c225181c173e436022833736f4baf639936f07b
Elly Maimon <eliranmaimon@gmail.com>
2018-07-13 12:43:32 -0700

    Add GetAll, DeleteAll, and Save methods to stylist.cs

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -13,7 +13,7 @@ namespace HairSalon.Models
         public string Specialty { get; set; }
         public string Phone { get; set; }
 
-        public Stylist (string name, int experience, string specialty, string phone, int id = 0)
+        public Stylist(string name, int experience, string specialty, string phone, int id = 0)
         {
             Name = name;
             Experience = experience;
@@ -36,6 +36,70 @@ namespace HairSalon.Models
                 bool specialtyEquality = (this.Specialty == newStylist.Specialty);
                 bool phoneEquality = (this.Phone == newStylist.Phone);
                 return (nameEquality && experienceEquality && specialtyEquality && phoneEquality);
+            }
+        }
+
+        public static List<Stylist> GetAll()
+        {
+            List<Stylist> allStylists = new List<Stylist>() { };
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM stylists;";
+            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while (rdr.Read())
+            {
+                int id = rdr.GetInt32(0);
+                string name = rdr.GetString(1);
+                int experience = rdr.GetInt32(2);
+                string specialty = rdr.GetString(3);
+                string phone = rdr.GetString(4);
+                Stylist newStylist = new Stylist(name, experience, specialty, phone, id);
+                allStylists.Add(newStylist);
+            }
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return allStylists;
+        }
+
+        public static void DeleteAll()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM stylists;";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = @"DELETE FROM clients;";
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public void Save()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"INSERT INTO stylists (name, experience, specialty, phone) VALUES (@Name, @Experience, @Specialty, @Phone);";
+            cmd.Parameters.AddWithValue("@Name", this.Name);
+            cmd.Parameters.AddWithValue("@Experience", this.Experience);
+            cmd.Parameters.AddWithValue("@Specialty", this.Specialty);
+            cmd.Parameters.AddWithValue("@Phone", this.Phone);
+            cmd.ExecuteNonQuery();
+            this.Id = (int)cmd.LastInsertedId;
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
             }
         }
     }
