d76218e423cca2ee95cc632437725d50bc869219
Elly Maimon <eliranmaimon@gmail.com>
2018-07-13 12:44:12 -0700

    Add and pass tests for GetAll, DeleteAll, and Save methods for Stylist.cs

--- HairSalon.Tests/ModelTests/Stylist.Tests.cs
+++ HairSalon.Tests/ModelTests/Stylist.Tests.cs
@@ -9,15 +9,9 @@ namespace HairSalon.Tests
     public class StylistTest : IDisposable
     {
 
-        public void Dispose()
-        {
-            // Stylist.DeleteAll();
-        }
+        public void Dispose() => Stylist.DeleteAll();
 
-        public StylistTest()
-        {
-            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=eliran_maimon_test;";
-        }
+        public StylistTest() => DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=eliran_maimon_test;";
 
         [TestMethod]
         public void GetSetProperties_GetsSetsProperties_True()
@@ -33,6 +27,23 @@ namespace HairSalon.Tests
             Stylist firstStylist = new Stylist("Elly Maimon", 3, "Coloring", "513-213-0982");
             Stylist secondStylist = new Stylist("Elly Maimon", 3, "Coloring", "513-213-0982");
             Assert.AreEqual(firstStylist, secondStylist);
+        }
+
+        [TestMethod]
+        public void GetAll_DbStartsEmpty_0()
+        {
+            int result = Stylist.GetAll().Count;
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Save_SavesToDatabase_StylistList()
+        {
+            Stylist newStylist = new Stylist("Elly Maimon", 3, "Coloring", "513-213-0982");
+            newStylist.Save();
+            List<Stylist> actualList = Stylist.GetAll();
+            List<Stylist> expectedList = new List<Stylist> { newStylist };
+            CollectionAssert.AreEqual(actualList, expectedList);
         }
     }
 }
