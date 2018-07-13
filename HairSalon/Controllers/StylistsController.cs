3b3c92e3c37a108bd276d5a8c0565df5f0381323
Elly Maimon <eliranmaimon@gmail.com>
2018-07-13 12:43:09 -0700

    Add stylist controller

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+
+namespace HairSalon.Controllers
+{
+    public class StylistsController : Controller
+    {
+        // [HttpGet("/stylists")]
+        // public ActionResult Index()
+        // {
+        //     // // List<Stylist> allStylists = Stylist.GetAll();
+        //     // return View(allStylists);
+        // }
+    }
+}
