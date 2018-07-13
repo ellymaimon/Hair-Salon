437d3b66675c5487564f97a06fd246ae4b27560f
Elly Maimon <eliranmaimon@gmail.com>
2018-07-13 14:53:36 -0700

    Add and pass tests for update and delete methods

--- HairSalon.Tests/ModelTests/Stylist.Tests.cs
+++ HairSalon.Tests/ModelTests/Stylist.Tests.cs
@@ -55,5 +55,41 @@ namespace HairSalon.Tests
             Stylist foundStylist = Stylist.Find(newStylist.Id);
             Assert.AreEqual(newStylist, foundStylist);
         }
+
+        [TestMethod]
+        public void Delete_DeletesRecordFromDatabase_StylistList()
+        {
+            Stylist newStylist = new Stylist("Elly Maimon", "3 to 6", "Coloring", "513-213-0982");
+            Stylist newStylist2 = new Stylist("Carrie Zamboni", "10+", "All-Around", "555-222-1111");
+            newStylist.Id = 1;
+            newStylist2.Id = 2;
+            newStylist.Save();
+            newStylist2.Save();
+
+            // Client newClient = new Client("Jojo Peters", "Male", newStylist.Id);
+            // newClient.Save();
+
+            newStylist.Delete();
+
+            List<Stylist> actualList = Stylist.GetAll();
+            List<Stylist> expectedList = new List<Stylist>{ newStylist2 };
+            // List<Client> clientResult = Client.GetAll();
+            // List<Client> clientTest = new List<Client>() {};
+            CollectionAssert.AreEqual(actualList, expectedList);
+            // CollectionAssert.AreEqual(clientResult, clientTest);
+        }   
+
+        [TestMethod]
+        public void Update_UpdatesColumnInDatabase_StylistList()
+        {
+            Stylist newStylist = new Stylist("Elly Maimon", "3 to 6", "Coloring", "513-213-0982");
+            newStylist.Id = 1;
+            newStylist.Save();
+            newStylist.Name = "Carrie Zamboni";
+            newStylist.Update();
+            List<Stylist> actualList = Stylist.GetAll();
+            List<Stylist> expectedList = new List<Stylist>{ newStylist };
+            CollectionAssert.AreEqual(expectedList, actualList);
+        }        
     }
 }
