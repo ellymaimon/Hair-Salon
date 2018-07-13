aa5ab380b088b971e151e4845bc941122196b092
Elly Maimon <eliranmaimon@gmail.com>
2018-07-13 12:50:10 -0700

    Add index for stylist controller, test view for stylist controller index, pass test

---
+++ HairSalon.Tests/ControllerTests/StylistsControllerTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using HairSalon.Controllers;
+using HairSalon.Models;
+
+namespace HairSalon.Tests
+{
+    [TestClass]
+    public class StylistsControllerTest
+    {
+        [TestMethod]
+        public void Index_ReturnsCorrectView_True()
+        {
+            StylistsController controller = new StylistsController();
+            ActionResult indexView = controller.Index();
+            Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+        }
+
+    }
+}
+
+
+
+
--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -6,11 +6,11 @@ namespace HairSalon.Controllers
 {
     public class StylistsController : Controller
     {
-        // [HttpGet("/stylists")]
-        // public ActionResult Index()
-        // {
-        //     // // List<Stylist> allStylists = Stylist.GetAll();
-        //     // return View(allStylists);
-        // }
+        [HttpGet("/stylists")]
+        public ActionResult Index()
+        {
+            List<Stylist> allStylists = Stylist.GetAll();
+            return View(allStylists);
+        }
     }
 }
