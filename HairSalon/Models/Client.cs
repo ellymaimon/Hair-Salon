b741b7e61f4ebe5d798d9c793756521a47cac71d
Elly Maimon <eliranmaimon@gmail.com>
2018-07-13 11:59:23 -0700

    Fix Client.cs file

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -7,24 +7,5 @@ namespace HairSalon.Models
 {
     public class Client
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Gender { get; set; }
-        public string ImageName { get; set; }
-        public int StylistId { get; set; }
-    }
-
-    public override bool Equals(System.Object otherClient)
-    {
-        if (!(otherClient is Client))
-        {
-            return false;
-        }
-        else
-        {
-            Client newClient = (Client)otherClient;
-            bool nameEquality = (this.Name == newClient.Name);
-            return (nameEquality);
-        }
     }
 }
