901c91bec276e4cfebe7a93295dc8670ea39cc4d
Elly Maimon <eliranmaimon@gmail.com>
2018-07-13 12:00:42 -0700

    Add and pass get and set properties test, add and pass override equals test for stylist class

--- HairSalon.Tests/ModelTests/Stylist.Tests.cs
+++ HairSalon.Tests/ModelTests/Stylist.Tests.cs
@@ -6,8 +6,33 @@ using HairSalon.Models;
 namespace HairSalon.Tests
 {
     [TestClass]
-    public class StylistTest
+    public class StylistTest : IDisposable
     {
 
+        public void Dispose()
+        {
+            // Stylist.DeleteAll();
+        }
+
+        public StylistTest()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=eliran_maimon_test;";
+        }
+
+        [TestMethod]
+        public void GetSetProperties_GetsSetsProperties_True()
+        {
+            Stylist newStylist = new Stylist("Elly Maimon", 3, "Coloring", "513-213-0982");
+            newStylist.Name = "Carrie Zamboni";
+            Assert.AreEqual("Carrie Zamboni", newStylist.Name);
+        }
+
+        [TestMethod]
+        public void Equals_ReturnsTrueIfNamesAreTheSame_Stylist()
+        {
+            Stylist firstStylist = new Stylist("Elly Maimon", 3, "Coloring", "513-213-0982");
+            Stylist secondStylist = new Stylist("Elly Maimon", 3, "Coloring", "513-213-0982");
+            Assert.AreEqual(firstStylist, secondStylist);
+        }
     }
 }
